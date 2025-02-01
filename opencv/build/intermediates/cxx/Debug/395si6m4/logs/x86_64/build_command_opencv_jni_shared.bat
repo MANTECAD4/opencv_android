ee7e76e59fe58e874b94dcb505b39f2dd47c0d6b
DanielMtzz <daniel.martinez5659@alumnos.udg.mx>
2025-02-01 15:36:03 -0600

    Delete modulo innecesario

--- opencv/build/intermediates/cxx/Debug/395si6m4/logs/x86_64/build_command_opencv_jni_shared.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-"C:\\Users\\kangr\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
-  -C ^
-  "C:\\Users\\kangr\\Desktop\\servicio\\filtros\\opencv\\.cxx\\Debug\\395si6m4\\x86_64" ^
-  opencv_jni_shared
